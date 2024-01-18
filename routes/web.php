d39574770432266d1b29c17817b276c05a51795c
Waqas Ahmed Khan <vikikhan88@gmail.com>
2024-01-18 21:25:44 +1100

    Initialization 1. Laravel 2. Setup Docker 3. Composer installation 4. Tailwind Css include into the project

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Auth::routes();
+
+Route::get('/', function () {
+    return view('auth/login');
+});
+
+Route::get('/products', function () {
+    return view('pages/products/productsList');
+});
+Route::get('/products/details/{code}', function () {
+    return view('pages/products/productDetails');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// tailwind.config.js
+import typography from '@tailwindcss/typography';
+import forms from '@tailwindcss/forms';
+import aspectRatio from '@tailwindcss/aspect-ratio';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+  ],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
+  },
+  plugins: [
+    typography,
+    forms,
+    aspectRatio,
+  ],
+}
